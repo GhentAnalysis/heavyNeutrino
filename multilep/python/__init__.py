f5a91be56268bcc640ef757ada4a6b5b59bd98fd
UAEDF-tomc <tom.cornelis@ua.ac.be>
2017-07-26 14:11:33 +0200

    remove __init__.py from tracked files

--- multilep/python/__init__.py
+++
@@ -1,3 +0,0 @@
-#Automatically created by SCRAM
-import os
-__path__.append(os.path.dirname(os.path.abspath(__file__).rsplit('/testAnalyzer/multilep/',1)[0])+'/cfipython/slc6_amd64_gcc493/testAnalyzer/multilep')
