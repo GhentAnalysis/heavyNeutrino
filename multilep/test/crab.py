97fa1bd13318c7e6364dcf76e8a635f0f1c3923d
UAEDF-tomc <tom.cornelis@ua.ac.be>
2017-07-27 15:02:38 +0200

    simplify crab directory name

--- multilep/test/crab.py
+++ multilep/test/crab.py
@@ -6,12 +6,18 @@ productionLabel = os.environ['CRAB_PRODUCTIONLABEL']
 dataset         = os.environ['CRAB_DATASET']
 outputFile      = os.environ['CRAB_OUTPUTFILE']
 
+requestName = dataset.split('/')[2] + '_' + productionLabel
+requestName = requestName.replace('RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6','Moriond2017')
+requestName = requestName.replace('RunIISummer16MiniAODv2-PUMoriond17_HCALDebug_80X_mcRun2_asymptotic_2016_TrancheIV_v6','Moriond2017')
+requestName = requestName.replace('RunIISummer16MiniAODv2-80X_mcRun2_asymptotic_2016_TrancheIV_v6', '80X')
+
+
 # Crab configuration
 config = Configuration()
 config.section_('General')
 config.General.transferLogs            = True
-config.General.requestName             = dataset.split('/')[2].replace('80X_mcRun2_asymptotic_2016_TrancheIV_v6','') + '_' + productionLabel
-config.General.workArea                = os.path.join('crab', productionLabel, dataset.split('/')[1] + dataset.split('/')[2])
+config.General.requestName             = requestName + '_' + productionLabel
+config.General.workArea                = os.path.join('crab', productionLabel, dataset.split('/')[1])
 
 config.section_('JobType')
 config.JobType.psetName                = 'multilep.py'
