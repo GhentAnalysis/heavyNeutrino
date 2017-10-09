c408a437cee38095536e1dd103bf5b4a229623fb
martinavit <martina.vit@cern.ch>
2017-10-09 18:10:26 +0200

    fix bugs

--- multilep/interface/LeptonAnalyzer.h
+++ multilep/interface/LeptonAnalyzer.h
@@ -79,6 +79,7 @@ class LeptonAnalyzer {
     bool _lPOGLoose[nL_max];
     bool _lPOGMedium[nL_max];
     bool _lPOGTight[nL_max];
+    bool _lPFMuon[nL_max];
   
     double _eleNumberInnerHitsMissing[nL_max];
     double _muNumberInnerHits[nL_max];
