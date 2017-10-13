d6f5968ad4ed3444c29b878f3bc0085b3c664304
willem <willem.verbeke@cern.ch>
2017-10-13 15:11:57 +0200

    Completed SUSYMassAnalyzer interface

--- multilep/interface/SUSYMassAnalyzer.h
+++ multilep/interface/SUSYMassAnalyzer.h
@@ -5,7 +5,16 @@
 class multilep;
 
 class SUSYMassAnalyzer{
-
-
-
+    private:
+        TH1D*  hCounterSUSY;        //Event counter for every mass point in the scan sample
+        multilep* multilepAnalyzer;
+        
+        //EWKino masses
+        double _mChi0;              //LSP (neutralino1) mass
+        double _mChi1;              //Chargino mass = neutralino2 mass
+    public:
+        SUSYMassAnalyzer(const edm::ParameterSet& iConfig, multilep* vars);
+        ~SUSYMassAnalyzer();
+        void beginJob(TTree* outputTree, edm::Service<TFileService>& fs);
+        void analyze(const edm::Event&);
 };
