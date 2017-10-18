2e89241b1a8ce060fa533968407dc9b047af2247
willem <willem.verbeke@cern.ch>
2017-10-18 14:19:32 +0200

    edit resubmission script

--- multilep/test/resubmitCrab.sh
+++ multilep/test/resubmitCrab.sh
@@ -6,4 +6,11 @@ checkCrab(){
         fi
     done
 }
-checkCrab $1
+if [[ -z $1 ]] 
+    echo "Empty string given, will resubmit ALL failed crab jobs"
+    then for d in ./crab
+        do checkCrab $d
+    done
+else 
+    checkCrab $1
+fi
