caab9e547cf956f5cae85d8a2aa58fa7dbb63361
willem <willem.verbeke@cern.ch>
2017-10-18 14:30:19 +0200

    fix resubmission script

--- multilep/test/resubmitCrab.sh
+++ multilep/test/resubmitCrab.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 checkCrab(){
-    for dir in ./*/*;
+    for dir in ${1}/*/*;
         do if [[ $dir != *"done"* ]]
             then echo $dir 
-            ./crabStatus.py $dir
+            ${CMSSW_BASE}/src/heavyNeutrino/multilep/test/crabStatus.py $dir
         fi
     done
 }
 if [[ -z $1 ]] 
-    echo "Empty string given, will resubmit ALL failed crab jobs"
-    then for d in ./crab/*
-        do checkCrab $d
+    then echo "Empty string given, will resubmit ALL failed crab jobs"
+    for d in ./crab/*
+        do if [[ -d $d ]]
+            then checkCrab $d
+        fi
     done
 else 
     checkCrab $1
