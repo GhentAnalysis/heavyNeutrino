11fcffecf5fd116ca00caa0816cc1e02039c9c8b
willem <willem.verbeke@cern.ch>
2017-10-23 14:50:18 +0200

    Reconstruct CMSDAS name from pnfs directory if local files are given

--- multilep/test/submitAll.py
+++ multilep/test/submitAll.py
@@ -17,7 +17,17 @@ for dataset in datasets:
   if 'pnfs' in dataset or 'user' in dataset or submitLocal: 
       dir        = os.getcwd()
       outputDir = outDir + dataset.split('/')[-1]      
-      #find way to give skim conditon (determined by outPutName) to bash script
+      #cut out the first part of /pnfs path for official sample if needed
+      if 'pnfs' in dataset:
+        dataset = dataset.replace("/pnfs/iihe/cms/ph/sc4/store/mc", "")
+        #naming in pnfs directories is slightly altered compared to the CMSDAS name, rever this to the CMSDAS version:
+        period = dataset[1:].split('/')[0]
+        name   = dataset[1:].split('/')[1]
+        form   = dataset[1:].split('/')[2]
+        puScen = dataset[1:].split('/')[3]
+        dataset = '/' + name + '/' + period + '-' + puScen + '/' + form
+
+      print dataset
       os.system('bash runLocal.sh ' + dataset + ' ' + outputDir + ' ' + outputName + ' ' + filesPerJob)
   else: # use crab
     print 'Submitting ' + dataset + ' using crab:'
