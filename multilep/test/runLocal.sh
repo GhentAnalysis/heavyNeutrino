0959b758cef9363e9a9178a5e248c7f4277f7b4c
willem <willem.verbeke@cern.ch>
2017-10-23 14:51:21 +0200

    fix stupid typo

--- multilep/test/runLocal.sh
+++ multilep/test/runLocal.sh
@@ -60,10 +60,10 @@ while read f
         #initialize CMSSW environment in submission script
         setCMSSW $1
     fi
-    echo "cmsRun ./heavyNeutrino/multilep/test/multilep.py dir=$output , inputFile=$f, outputFile=${output}/Job_${count}_${skim}.root, events=-1 > ${output}/logs/Job_${count}.txt 2> ${output}/errs/Job_${count}.txt" >> $submitS
+    echo "cmsRun ./heavyNeutrino/multilep/test/multilep.py dir=$output , inputFile=$f, outputFile=${output}/Job_${count}_${skim}.root, events=-1 > ${output}/logs/Job_${count}.txt 2> ${output}/errs/Job_${count}.txt" >> $submit
     count=$((count + 1))
 done < fileList.txt
 #qsub $submit -l walltime=40:00:00;
 cat $submit
 #remove temporary files
-rm fileList.txt
+#rm fileList.txt
