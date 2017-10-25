66842c2cadd51ccb7cc5901dc574b8507d33a013
willem <willem.verbeke@cern.ch>
2017-10-25 14:55:56 +0200

    Fix runLocal script

--- multilep/test/runLocal.sh
+++ multilep/test/runLocal.sh
@@ -53,6 +53,7 @@ while read f
     do if (( $count % $filesPerJob == 0 ))
         then if (( $count != 0)) 
             then qsub $submit -l walltime=40:00:00;
+            #then cat $submit
         fi
         #initialize temporary submission script
         if [ -e $submit ]; then rm $submit; fi
@@ -60,9 +61,10 @@ while read f
         #initialize CMSSW environment in submission script
         setCMSSW $submit
     fi
-    echo "cmsRun ./heavyNeutrino/multilep/test/multilep.py dir=$output, inputFile=$f, outputFile=${output}/Job_${count}_${skim}.root, events=-1 > ${output}/logs/Job_${count}.txt 2> ${output}/errs/Job_${count}.txt" >> $submit
+    echo "cmsRun ${CMSSW_BASE}/src/heavyNeutrino/multilep/test/multilep.py inputFile=$f outputFile=${output}/Job_${count}_${skim}.root events=-1 > ${output}/logs/Job_${count}.txt 2> ${output}/errs/Job_${count}.txt" >> $submit
     count=$((count + 1))
 done < fileList.txt
 qsub $submit -l walltime=40:00:00;
+#cat $submit
 #remove temporary files
 rm fileList.txt
