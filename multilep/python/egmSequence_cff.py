e54713378b7ae02d1c323a1e861d05afe1838330
Illia Khvastunov <illia.khvastunov@cern.ch>
2017-11-29 19:52:30 +0100

    remove all related to jet ucnertainties

--- multilep/python/egmSequence_cff.py
+++ multilep/python/egmSequence_cff.py
@@ -8,13 +8,14 @@ def addElectronAndPhotonSequence(process):
   #
   process.load("Configuration.StandardSequences.GeometryRecoDB_cff")
   switchOnVIDElectronIdProducer(process, DataFormat.MiniAOD)
-  switchOnVIDPhotonIdProducer(  process, DataFormat.MiniAOD)
+  #switchOnVIDPhotonIdProducer(  process, DataFormat.MiniAOD)
   electronModules = ['RecoEgamma.ElectronIdentification.Identification.mvaElectronID_Spring16_GeneralPurpose_V1_cff',
                      'RecoEgamma.ElectronIdentification.Identification.mvaElectronID_Spring16_HZZ_V1_cff',
                      'RecoEgamma.ElectronIdentification.Identification.cutBasedElectronID_Summer16_80X_V1_cff']
-  photonModules   = ['RecoEgamma.PhotonIdentification.Identification.mvaPhotonID_Spring16_nonTrig_V1_cff',
-                     'RecoEgamma.PhotonIdentification.Identification.cutBasedPhotonID_Spring16_V2p2_cff']
+  #photonModules   = ['RecoEgamma.PhotonIdentification.Identification.mvaPhotonID_Spring16_nonTrig_V1_cff',
+  #                   'RecoEgamma.PhotonIdentification.Identification.cutBasedPhotonID_Spring16_V2p2_cff']
   for idmod in electronModules: setupAllVIDIdsInModule(process,idmod,setupVIDElectronSelection)
-  for idmod in photonModules:   setupAllVIDIdsInModule(process,idmod,setupVIDPhotonSelection)
+  #for idmod in photonModules:   setupAllVIDIdsInModule(process,idmod,setupVIDPhotonSelection)
 
-  process.egmSequence = cms.Sequence(process.egmGsfElectronIDSequence * process.egmPhotonIDSequence)
+  #process.egmSequence = cms.Sequence(process.egmGsfElectronIDSequence * process.egmPhotonIDSequence)
+  process.egmSequence = cms.Sequence(process.egmGsfElectronIDSequence)
