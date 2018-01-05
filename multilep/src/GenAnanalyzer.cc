GhentAnalysis/heavyNeutrino

commit 5cb442897f21378905a2a37fa2bfeab8f5754d95

refurbish and optimize genAnalyzer using new functions

multilep/src/GenAnanalyzer.cc
@@ -1,14 +1,15 @@
-#include "heavyNeutrino/multilep/interface/GenAnalyzer.h"
+//include CMSSW classes
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
-
 #include "DataFormats/HepMCCandidate/interface/GenParticle.h"
 
-//TlorentzVector, useful for calculating generator MET
+//include ROOT classes
 #include "TLorentzVector.h"
+
+//include other parts of code 
+#include "heavyNeutrino/multilep/interface/GenAnalyzer.h"
+#include "heavyNeutrino/multilep/interface/GenTools.h"
 /*
  * Storing generator particles
- * Currently storing everything such that we have it available for studies on tuple level
- * Might consider trimming it down to save space
  */
 
 
@@ -74,12 +75,12 @@ void GenAnalyzer::analyze(const edm::Event& iEvent){
                 _gen_lE[_gen_nL]        = p.energy();
                 _gen_lCharge[_gen_nL]   = p.charge();
                 _gen_lIsPrompt[_gen_nL] = (p.isPromptDecayed() || p.isPromptFinalState());
-                _gen_lMomPdg[_gen_nL]   = getMotherPdgId(p, *genParticles);
+                _gen_lMomPdg[_gen_nL]   = GenTools::getMother(p, *genParticles)->pdgId();
 
-                std::vector<int> motherList = {};
-                getMotherList(p, *genParticles, motherList);
+                std::set<int> decayChain;
+                GenTools::setDecayChain(p, *genParticles, decayChain);
                 _gen_lMinDeltaR[_gen_nL]     = getMinDeltaR(p, *genParticles);
-                _gen_lPassParentage[_gen_nL] = !(*(std::max_element(std::begin(motherList), std::end(motherList))) > 37 or *(std::min_element(std::begin(motherList), std::end(motherList))) < -37);
+                _gen_lPassParentage[_gen_nL] = !(*(std::max_element(std::begin(decayChain), std::end(decayChain))) > 37 or *(std::min_element(std::begin(decayChain), std::end(decayChain))) < -37);
 
                 if(abs(p.pdgId()) == 11)      _gen_lFlavor[_gen_nL] = 0;
                 else if(abs(p.pdgId()) == 13) _gen_lFlavor[_gen_nL] = 1;
@@ -91,17 +92,18 @@ void GenAnalyzer::analyze(const edm::Event& iEvent){
         //store generator level photon info
         if((p.status() == 1 || p.status() == 71) && abs(p.pdgId()) == 22){
             if(_gen_nPh != gen_nPh_max){
-                std::vector<int> motherList = {};
-                getMotherList(p, *genParticles, motherList);
                 _gen_phStatus[_gen_nPh]        = p.status();
                 _gen_phPt[_gen_nPh]            = p.pt();
                 _gen_phEta[_gen_nPh]           = p.eta();
                 _gen_phPhi[_gen_nPh]           = p.phi();
                 _gen_phE[_gen_nPh]             = p.energy();
                 _gen_phIsPrompt[_gen_nPh]      = p.isPromptFinalState();
-                _gen_phMomPdg[_gen_nPh]        = getMotherPdgId(p, *genParticles);
+                _gen_phMomPdg[_gen_nPh]        = GenTools::getMother(p, *genParticles)->pdgId();
                 _gen_phMinDeltaR[_gen_nPh]     = getMinDeltaR(p, *genParticles);
-                _gen_phPassParentage[_gen_nPh] = !(*(std::max_element(std::begin(motherList), std::end(motherList))) > 37 or *(std::min_element(std::begin(motherList), std::end(motherList))) < -37);
+                std::set<int> decayChain;
+                GenTools::setDecayChain(p, *genParticles, decayChain);
+                _gen_phMinDeltaR[_gen_nPh]     = getMinDeltaR(p, *genParticles);
+                _gen_phPassParentage[_gen_nPh] = !(*(std::max_element(std::begin(decayChain), std::end(decayChain))) > 37 or *(std::min_element(std::begin(decayChain), std::end(decayChain))) < -37);
                 ++_gen_nPh;
             } 
         }
@@ -132,31 +134,6 @@ double GenAnalyzer::getMinDeltaR(const reco::GenParticle& p, const std::vector<r
     return minDeltaR;
 }
 
-
-const reco::GenParticle* GenAnalyzer::getMother(const reco::GenParticle& p, const std::vector<reco::GenParticle>& genParticles){
-    return (p.numberOfMothers() == 0) ? nullptr : &genParticles[p.motherRef(0).key()];
-}
-
-const int GenAnalyzer::getMotherPdgId(const reco::GenParticle& p, const std::vector<reco::GenParticle>& genParticles){
-    const reco::GenParticle* mom = getMother(p, genParticles);
-    if(!mom)                         return 0;
-    else if(mom->pdgId()==p.pdgId()) return getMotherPdgId(*mom, genParticles);
-    else                             return mom->pdgId();
-}
-
-// Make a (recursive) list of ancestors of a particle, taking out copies and protons
-void GenAnalyzer::getMotherList(const reco::GenParticle& p, const std::vector<reco::GenParticle>& genParticles, std::vector<int>& list){
-    if((list.empty() or p.pdgId() != list.back()) and p.pdgId() != 2212) list.push_back(p.pdgId());
-    if(p.numberOfMothers() > 1) getMotherList(genParticles[p.motherRef(1).key()], genParticles, list);
-    if(p.numberOfMothers() > 0) getMotherList(genParticles[p.motherRef(0).key()], genParticles, list);
-}
-
-
-bool GenAnalyzer::inMotherList(std::vector<int>& list, int i){
-    return (std::find(list.begin(), list.end(), i) != list.end());
-}
-
-
 /*
  * Some event categorization in order to understand/debug/apply overlap removal for TTG <--> TTJets
  */
@@ -170,21 +147,20 @@ unsigned GenAnalyzer::ttgEventType(const std::vector<reco::GenParticle>& genPart
         if(fabs(p->eta())>etaCut) continue;
         type = std::max(type, 2);                                                            // Type 2: photon from pion or other meson
 
-        std::vector<int> motherList = {};
-        getMotherList(*p, genParticles, motherList);
-
-        if(*(std::max_element(std::begin(motherList), std::end(motherList))) > 37)  continue;
-        if(*(std::min_element(std::begin(motherList), std::end(motherList))) < -37) continue;
-
+        std::set<int> decayChain;
+        GenTools::setDecayChain(*p, genParticles, decayChain);
+        if(*(std::max_element(std::begin(decayChain), std::end(decayChain))) > 37)  continue;
+        if(*(std::min_element(std::begin(decayChain), std::end(decayChain))) < -37) continue;
         // Everything below is *signal*
-        if(inMotherList(motherList, 24) or inMotherList(motherList, -24)){                   // If a W-boson in ancestry
-            if(abs(getMotherPdgId(*p, genParticles)) == 24)     type =std::max(type, 6);       // Type 6: photon directly from W or decay products which are part of ME
-            else if(abs(getMotherPdgId(*p, genParticles)) <= 6) type =std::max(type, 4);       // Type 4: photon from quark from W (photon from pythia, rarely)
-            else                                                type =std::max(type, 5);       // Type 5: photon from lepton from W (photon from pythia)
+        const reco::GenParticle* mom = GenTools::getMother(*p, genParticles);
+        if(std::find_if(decayChain.cbegin(), decayChain.cend(), [](const int entry) { return abs(entry) == 24; }) != decayChain.cend() ){
+            if( abs(mom->pdgId()) == 24 )    type = std::max(type, 6);      // Type 6: photon directly from W or decay products which are part of ME
+            else if( abs(mom->pdgId()) <= 6) type =std::max(type, 4);       // Type 4: photon from quark from W (photon from pythia, rarely)
+            else                             type =std::max(type, 5);       // Type 5: photon from lepton from W (photon from pythia)
         } else {
-            if(abs(getMotherPdgId(*p, genParticles)) == 6)      type = std::max(type, 7);      // Type 7: photon from top
-            else if(abs(getMotherPdgId(*p, genParticles)) == 5) type = std::max(type, 3);      // Type 3: photon from b
-            else                                                type = std::max(type, 8);      // Type 8: photon from ME
+            if(abs(mom->pdgId()) == 6)      type = std::max(type, 7);      // Type 7: photon from top
+            else if(abs(mom->pdgId()) == 5) type = std::max(type, 3);      // Type 3: photon from b
+            else                            type = std::max(type, 8);      // Type 8: photon from ME
         }
     }
     return type;

multilep/src/GenTools.cc
@@ -11,35 +11,35 @@ const reco::GenParticle* GenTools::getMother(const reco::GenParticle& gen, const
     else return mom;
 }
  
-std::set<unsigned> GenTools::getDecayChain(const reco::GenParticle& gen, const std::vector<reco::GenParticle>& genParticles, std::set<unsigned>& list){
-    if(list.empty() && gen.pdgId() != 2212) list.insert(gen.pdgId());
-    if(gen.numberOfMothers() > 1) getDecayChain(genParticles[gen.motherRef(1).key()], genParticles, list);
-    if(gen.numberOfMothers() > 0) getDecayChain(genParticles[gen.motherRef(0).key()], genParticles, list);
-    return list;
+void GenTools::setDecayChain(const reco::GenParticle& gen, const std::vector<reco::GenParticle>& genParticles, std::set<int>& list){
+    if(list.empty() && gen.pdgId() != 2212 && list.find(gen.pdgId()) == list.end()) list.insert(gen.pdgId());
+    if(gen.numberOfMothers() > 1) setDecayChain(genParticles[gen.motherRef(1).key()], genParticles, list);
+    if(gen.numberOfMothers() > 0) setDecayChain(genParticles[gen.motherRef(0).key()], genParticles, list);
 }
 
-bool GenTools::bosonInChain(const std::set<unsigned>& chain){
-   return std::find_if(chain.cbegin(), chain.cend(), [](const unsigned entry){ return abs(entry) > 22 && abs(entry) < 26; }) != chain.cend();
+bool GenTools::bosonInChain(const std::set<int>& chain){
+   return std::find_if(chain.cbegin(), chain.cend(), [](const int entry){ return abs(entry) > 22 && abs(entry) < 26; }) != chain.cend();
 }
  
-bool GenTools::bBaryonInChain(const std::set<unsigned>& chain){
-   return std::find_if(chain.cbegin(), chain.cend(), [](const unsigned entry){ return (entry/1000)%10 == 5; }) != chain.cend();
+bool GenTools::bBaryonInChain(const std::set<int>& chain){
+   return std::find_if(chain.cbegin(), chain.cend(), [](const int entry){ return (entry/1000)%10 == 5; }) != chain.cend();
 }
 
-bool GenTools::bMesonInChain(const std::set<unsigned>& chain){
-   return std::find_if(chain.cbegin(), chain.cend(), [](const unsigned entry){ unsigned mod = entry%1000; return mod >= 500 && mod <= 600; }) != chain.cend();
+bool GenTools::bMesonInChain(const std::set<int>& chain){
+   return std::find_if(chain.cbegin(), chain.cend(), [](const int entry){ unsigned mod = entry%1000; return mod >= 500 && mod <= 600; }) != chain.cend();
 }
 
-bool GenTools::cBaryonInChain(const std::set<unsigned>& chain){
-   return std::find_if(chain.cbegin(), chain.cend(), [](const unsigned entry){return (entry/1000)%10 == 4; }) != chain.cend();
+bool GenTools::cBaryonInChain(const std::set<int>& chain){
+   return std::find_if(chain.cbegin(), chain.cend(), [](const int entry){return (entry/1000)%10 == 4; }) != chain.cend();
 }
 
-bool GenTools::cMesonInChain(const std::set<unsigned>& chain){
-   return std::find_if(chain.cbegin(), chain.cend(), [](const unsigned entry){ unsigned mod = entry%1000; return mod >= 400 && mod <= 500; }) != chain.cend();
+bool GenTools::cMesonInChain(const std::set<int>& chain){
+   return std::find_if(chain.cbegin(), chain.cend(), [](const int entry){ unsigned mod = entry%1000; return mod >= 400 && mod <= 500; }) != chain.cend();
 }
 
 unsigned GenTools::provenance(const reco::GenParticle& gen, const std::vector<reco::GenParticle>& genParticles){
-    std::set<unsigned> decayChain = getDecayChain(gen, genParticles, decayChain);
+    std::set<int> decayChain;
+    setDecayChain(gen, genParticles, decayChain);
     if(bMesonInChain(decayChain) || bBaryonInChain(decayChain) ) return 1;          //lepton from heavy flavor decay
     if(cMesonInChain(decayChain) || cBaryonInChain(decayChain) ) return 2;          //lepton from c flavor decay
     if(bosonInChain(decayChain) ) return 0;                                         //lepton from boson

multilep/interface/GenAnalyzer.h
@@ -54,11 +54,6 @@ class GenAnalyzer {
     double _gen_HT;
 
     //Functions to find the mother of a gen particle
-    const reco::GenParticle* getMother(const reco::GenParticle&, const std::vector<reco::GenParticle>&);
-    const int                getMotherPdgId(const reco::GenParticle&, const std::vector<reco::GenParticle>&);
-    void                     getMotherList(const reco::GenParticle&, const std::vector<reco::GenParticle>&, std::vector<int>&);
-    bool                     inMotherList(std::vector<int>& list, int i);
-
     unsigned                 ttgEventType(const std::vector<reco::GenParticle>& genParticles, double ptCut, double etaCut);
     double                   getMinDeltaR(const reco::GenParticle& p, const std::vector<reco::GenParticle>& genParticles);
 

multilep/interface/GenTools.h
@@ -29,12 +29,12 @@
 namespace GenTools{
     const reco::GenParticle* getFirstMother(const reco::GenParticle&, const std::vector<reco::GenParticle>&);
     const reco::GenParticle* getMother(const reco::GenParticle&, const std::vector<reco::GenParticle>&);
-    std::set<unsigned> getDecayChain(const reco::GenParticle& gen, const std::vector<reco::GenParticle>& genParticles, std::set<unsigned>& list);
-    bool bosonInChain(const std::set<unsigned>&);
-    bool bBaryonInChain(const std::set<unsigned>&);
-    bool bMesonInChain(const std::set<unsigned>&);
-    bool cBaryonInChain(const std::set<unsigned>&);
-    bool cMesonInChain(const std::set<unsigned>&);
+    void setDecayChain(const reco::GenParticle& gen, const std::vector<reco::GenParticle>& genParticles, std::set<int>& list);
+    bool bosonInChain(const std::set<int>&);
+    bool bBaryonInChain(const std::set<int>&);
+    bool bMesonInChain(const std::set<int>&);
+    bool cBaryonInChain(const std::set<int>&);
+    bool cMesonInChain(const std::set<int>&);
     unsigned provenance(const reco::GenParticle&, const std::vector<reco::GenParticle>&);
 }
 #endif

commit dcd7ee2ff1db9ead9e146c682bf87c58d151b311

fix bug introduced in 5cb442897f21378905a2a37fa2bfeab8f5754d95

multilep/src/GenAnanalyzer.cc
@@ -140,6 +140,7 @@ unsigned GenAnalyzer::ttgEventType(const std::vector<reco::GenParticle>& genPart
         GenTools::setDecayChain(*p, genParticles, decayChain);
         if(*(std::max_element(std::begin(decayChain), std::end(decayChain))) > 37)  continue;
         if(*(std::min_element(std::begin(decayChain), std::end(decayChain))) < -37) continue;
+
         // Everything below is *signal*
         const reco::GenParticle* mom = GenTools::getMother(*p, genParticles);
         if(std::find_if(decayChain.cbegin(), decayChain.cend(), [](const int entry) { return abs(entry) == 24; }) != decayChain.cend() ){

multilep/src/GenTools.cc
@@ -11,9 +11,9 @@ const reco::GenParticle* GenTools::getMother(const reco::GenParticle& gen, const
     else if(mom->pdgId() == gen.pdgId()) return getMother(*mom, genParticles);
     else return mom;
 }
- 
+
 void GenTools::setDecayChain(const reco::GenParticle& gen, const std::vector<reco::GenParticle>& genParticles, std::set<int>& list){
-    if( (list.empty() && gen.pdgId() != 2212) || list.find(gen.pdgId()) == list.end()) list.insert(gen.pdgId());
+    if((list.empty() or list.find(gen.pdgId())==list.end()) and gen.pdgId() != 2212) list.insert(gen.pdgId());
     if(gen.numberOfMothers() > 1) setDecayChain(genParticles[gen.motherRef(1).key()], genParticles, list);
     if(gen.numberOfMothers() > 0) setDecayChain(genParticles[gen.motherRef(0).key()], genParticles, list);
 }