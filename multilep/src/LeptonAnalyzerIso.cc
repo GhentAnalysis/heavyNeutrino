16de908daa20c19d43379dc9b11a845ec20c8849
martinavit <martina.vit@cern.ch>
2018-01-09 18:57:31 +0100

    Update LeptonAnalyzerIso.cc

--- multilep/src/LeptonAnalyzerIso.cc
+++ multilep/src/LeptonAnalyzerIso.cc
@@ -21,13 +21,8 @@ double LeptonAnalyzer::getRelIso03(const pat::Muon& mu, const double rho) const{
 	return absIso/mu.pt();
 }
 
-double LeptonAnalyzer::getRelIso04(const pat::Muon& mu, const double rho){ //Note: effective area correction is used instead of delta-beta correction
-	double puCorr = rho*muonsEffectiveAreas.getEffectiveArea(mu.eta());
-	double absIso = mu.pfIsolationR04().sumChargedHadronPt + std::max(0., mu.pfIsolationR04().sumNeutralHadronEt + mu.pfIsolationR04().sumPhotonEt - puCorr);
-	return absIso/mu.pt();
-}
 
-double LeptonAnalyzer::getRelIso03(const pat::Electron& ele, const double rho){
+double LeptonAnalyzer::getRelIso03(const pat::Electron& ele, const double rho) const{
 	double puCorr = rho*electronsEffectiveAreas.getEffectiveArea(ele.superCluster()->eta());
 	double absIso = ele.pfIsolationVariables().sumChargedHadronPt + std::max(0., ele.pfIsolationVariables().sumNeutralHadronEt + ele.pfIsolationVariables().sumPhotonEt - puCorr);
 	return absIso/ele.pt();
