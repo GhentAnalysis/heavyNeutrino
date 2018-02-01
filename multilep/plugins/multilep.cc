6c5730861c7a7089d9b9c48ff9b423bd72f2eaeb
martinavit <martina.vit@cern.ch>
2018-02-01 17:11:21 +0100

    Update multilep.cc

--- multilep/plugins/multilep.cc
+++ multilep/plugins/multilep.cc
@@ -114,8 +114,10 @@ void multilep::beginRun(const edm::Run& iRun, edm::EventSetup const& iSetup){
 
 // ------------ method called for each event  ------------
 void multilep::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup){
+    
     edm::Handle<std::vector<reco::Vertex>> vertices; iEvent.getByToken(vtxToken, vertices);
     edm::Handle<std::vector<pat::MET>> mets;         iEvent.getByToken(metToken, mets);
+    std::cout<<"=============================="<<std::endl;
     if(!isData) lheAnalyzer->analyze(iEvent);                          // needs to be run before selection to get correct uncertainties on MC xsection
     if(isSUSY) susyMassAnalyzer->analyze(iEvent);                      // needs to be run after LheAnalyzer, but before all other models
     if(!vertices->size()) return;                                      // don't consider 0 vertex events
