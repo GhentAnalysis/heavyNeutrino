0ee4b68baeb96cf0f6c19c6c96538bb38a615c04
UAEDF-tomc <tom.cornelis@ua.ac.be>
2018-02-02 11:22:09 +0100

    trying to add EGM smearer

--- setup.sh
+++ setup.sh
@@ -1,9 +1,17 @@
 if [[ $CMSSW_BASE == *CMSSW_8* ]]
 then
-  # Setup for new electron and photon MVA
   eval `scram runtime -sh`
   cd $CMSSW_BASE/src
+
+  # First EGM smearing
+  git cms-merge-topic cms-egamma:EGM_gain_v1
+  cd EgammaAnalysis/ElectronTools/data
+  git clone -b Moriond17_gainSwitch_unc https://github.com/ECALELFS/ScalesSmearings.git 
+  cd -
+
+  # Setup for new electron and photon MVA
   git cms-merge-topic ikrav:egm_id_80X_v3_photons
   scram b -j 10
+else
+  echo "Wrong branch! Setup script is made for CMSSW_8_X"
 fi
-# nothing to do for CMSSW_9_X
