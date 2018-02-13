04958ed52197b958d97563b852ae2392362cc44a
martinavit <martina.vit@cern.ch>
2018-02-13 17:09:01 +0100

    Update GenTools.cc

--- multilep/src/GenTools.cc
+++ multilep/src/GenTools.cc
@@ -13,11 +13,11 @@ const reco::GenParticle* GenTools::getMother(const reco::GenParticle& gen, const
 }
 
 void GenTools::setDecayChain(const reco::GenParticle& gen, const std::vector<reco::GenParticle>& genParticles, std::set<int>& list){
-     for(auto& i : list) std::cout << "list---> "<<i << std::endl;
+   //  for(auto& i : list) std::cout << "list---> "<<i << std::endl;
     if((list.empty() or list.find(gen.pdgId())==list.end()) and gen.pdgId() != 2212) list.insert(gen.pdgId());
     if(gen.numberOfMothers() > 1) setDecayChain(genParticles[gen.motherRef(1).key()], genParticles, list);
     if(gen.numberOfMothers() > 0) setDecayChain(genParticles[gen.motherRef(0).key()], genParticles, list);
-    for(auto& i : list) std::cout << "list  2   ---> "<<i << std::endl;
+  //  for(auto& i : list) std::cout << "list  2   ---> "<<i << std::endl;
 
 }
 
