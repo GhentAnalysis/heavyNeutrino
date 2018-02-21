382088fb5ec590694c464765a2bde20b3b38a7ba
willem <willem.verbeke@cern.ch>
2018-02-21 15:04:57 +0100

    only store loose jets

--- multilep/src/JetAnalyzer.cc
+++ multilep/src/JetAnalyzer.cc
@@ -49,6 +49,10 @@ bool JetAnalyzer::analyze(const edm::Event& iEvent){
         double unc = jecUnc.getUncertainty(true);
 
         if(std::max((1+unc)*jetSmeared->pt(), std::max(jetSmearedUp->pt(), jetSmearedDown->pt())) < 25) continue;
+        _jetId[_nJets]                    = jetId(*jet, false) + jetId(*jet, true); // 1: loose, 2: tight
+
+        //only store loose jets 
+        if(_jetId[_nJets] < 1 ) continue;
 
         _jetPt[_nJets]                    = jetSmeared->pt();
         _jetPt_JECDown[_nJets]            = jetSmeared->pt()*(1-unc);
@@ -67,7 +71,6 @@ bool JetAnalyzer::analyze(const edm::Event& iEvent){
         _jetDeepCsv_bb[_nJets]            = jet->bDiscriminator("pfDeepCSVJetTags:probbb");
         //  _jetDeepCsv_cc[_nJets]            = jet->bDiscriminator("pfDeepCSVJetTags:probcc");
         _jetHadronFlavor[_nJets]         = jet->hadronFlavour();
-        _jetId[_nJets]                    = jetId(*jet, false) + jetId(*jet, true); // 1: loose, 2: tight
         ++_nJets;
     }
     if(multilepAnalyzer->skim == "singlejet" and _nJets < 1) return false;
