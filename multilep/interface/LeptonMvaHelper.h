49d8b9bb840e4be9a87e757ae1327f80428ee56a
tomc <tomc@cern.ch>
2019-01-03 11:00:01 +0100

    Avoid tabs, only use spaces

--- multilep/interface/LeptonMvaHelper.h
+++ multilep/interface/LeptonMvaHelper.h
@@ -13,7 +13,7 @@ class LeptonMvaHelper{
     private:
         unsigned type; //0 = SUSY , 1 = ttH , 2 = tZqttV
         bool is2017;
-	    bool is2018;
+        bool is2018;
         std::shared_ptr<TMVA::Reader> reader[2]; //First entry is for muons, second one for electrons 
         float LepGood_pt,                       //Variables used in MVA computation
         LepGood_eta,
