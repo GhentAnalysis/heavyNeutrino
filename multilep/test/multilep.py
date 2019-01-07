518944648479b3210aa12e0fc5eec9e57a19c802
tuthongtran <44227844+tuthongtran@users.noreply.github.com>
2019-01-07 15:34:53 +0100

    Update multilep.py
    Typo

--- multilep/test/multilep.py
+++ multilep/test/multilep.py
@@ -51,7 +51,7 @@ process.TFileService = cms.Service("TFileService", fileName = cms.string(outputF
 
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 if is2018 and 'PromptReco' in inputFile: process.GlobalTag.globaltag = '102X_dataRun2_Prompt_v11'
-elif is2018:                             process.GlobalTag.globaltag = '102X_dataRun2_Sep2018Rereco_v1 'if isData else '102X_upgrade2018_realistic_v12' # TODO: GT for data not correct?
+elif is2018:                             process.GlobalTag.globaltag = '102X_dataRun2_Sep2018Rereco_v1' if isData else '102X_upgrade2018_realistic_v12' # TODO: GT for data not correct?
 elif is2017:                             process.GlobalTag.globaltag = '94X_dataRun2_v11'               if isData else '94X_mc2017_realistic_v17'
 else:                                    process.GlobalTag.globaltag = '94X_dataRun2_v10'               if isData else '94X_mcRun2_asymptotic_v3'
 
