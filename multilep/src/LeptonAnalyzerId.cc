ccf969bed861ae7a9f5cbb4e72e1c782143f5b35
tomc <tomc@cern.ch>
2019-01-08 17:21:12 +0100

    Removing Summer16 electron ID's without isolation --> from now on, use Fall17 and keep its scaled isolation cut; it is still possible to put additional multiIso on top if really needed

--- multilep/src/LeptonAnalyzerId.cc
+++ multilep/src/LeptonAnalyzerId.cc
@@ -30,55 +30,9 @@ bool LeptonAnalyzer::tauLightOverlap(const pat::Tau& tau, const bool* loose) con
 }
 
 /*
- * Manual electron Summer16 cut-based id without isolation
- * Based on https://twiki.cern.ch/twiki/bin/view/CMS/CutBasedLeptonAnalyzerRun2#Offline_selection_criteria
+ * Trigger emulation for single electron triggers is available in VID
+ * Trigger emulation for double electron triggers with CaloIdL_TrackIdL_IsoVL (is certainly outdated for everything past 2016)
  */
-float LeptonAnalyzer::dEtaInSeed(const pat::Electron* ele) const{
-    if(ele->superCluster().isNonnull() and ele->superCluster()->seed().isNonnull()) return ele->deltaEtaSuperClusterTrackAtVtx() - ele->superCluster()->eta() + ele->superCluster()->seed()->eta();
-    else                                                                            return std::numeric_limits<float>::max();
-}
-
-bool LeptonAnalyzer::isLooseCutBasedElectronWithoutIsolation(const pat::Electron* ele) const{
-    if(!(ele->isEB() or ele->isEE()))                                                               return false;
-    float eInvMinusPInv = fabs(1.0 - ele->eSuperClusterOverP())/ele->ecalEnergy();
-    if(ele->full5x5_sigmaIetaIeta()                  >= (ele->isEB() ? 0.11    : 0.0314 ))          return false;
-    if(fabs(dEtaInSeed(ele))                         >= (ele->isEB() ? 0.00477 : 0.00868))          return false;
-    if(fabs(ele->deltaPhiSuperClusterTrackAtVtx())   >= (ele->isEB() ? 0.222   : 0.213  ))          return false;
-    if(ele->hadronicOverEm()                         >= (ele->isEB() ? 0.298   : 0.101  ))          return false;
-    if(eInvMinusPInv                                 >= (ele->isEB() ? 0.241   : 0.14   ))          return false;
-    if(ele->gsfTrack()->hitPattern().numberOfLostHits(reco::HitPattern::MISSING_INNER_HITS) >  1)   return false;
-    if(!ele->passConversionVeto())                                                                  return false;
-    return true;
-}
-
-bool LeptonAnalyzer::isMediumCutBasedElectronWithoutIsolation(const pat::Electron* ele) const{
-    if(!(ele->isEB() or ele->isEE()))                                                               return false;
-    float eInvMinusPInv = fabs(1.0 - ele->eSuperClusterOverP())/ele->ecalEnergy();
-    if(ele->full5x5_sigmaIetaIeta()                  >= (ele->isEB() ? 0.00998    : 0.0298 ))       return false;
-    if(fabs(dEtaInSeed(ele))                         >= (ele->isEB() ? 0.00311    : 0.00609))       return false;
-    if(fabs(ele->deltaPhiSuperClusterTrackAtVtx())   >= (ele->isEB() ? 0.103      : 0.045  ))       return false;
-    if(ele->hadronicOverEm()                         >= (ele->isEB() ? 0.253      : 0.0878  ))      return false;
-    if(eInvMinusPInv                                 >= (ele->isEB() ? 0.134      : 0.13   ))       return false;
-    if(ele->gsfTrack()->hitPattern().numberOfLostHits(reco::HitPattern::MISSING_INNER_HITS) >  1)   return false;
-    if(!ele->passConversionVeto())                                                                  return false;
-    return true;
-}
-
-bool LeptonAnalyzer::isTightCutBasedElectronWithoutIsolation(const pat::Electron* ele) const{
-    if(!(ele->isEB() or ele->isEE())) return false;
-    float eInvMinusPInv = fabs(1.0 - ele->eSuperClusterOverP())/ele->ecalEnergy();
-    if(ele->full5x5_sigmaIetaIeta()                  >= (ele->isEB() ? 0.00998 : 0.0292))           return false;
-    if(fabs(dEtaInSeed(ele))                         >= (ele->isEB() ? 0.00308 : 0.00605))          return false;
-    if(fabs(ele->deltaPhiSuperClusterTrackAtVtx())   >= (ele->isEB() ? 0.0816  : 0.0394))           return false;
-    if(ele->hadronicOverEm()                         >= (ele->isEB() ? 0.0414  : 0.0641))           return false;
-    if(eInvMinusPInv                                 >= (ele->isEB() ? 0.0129  : 0.0129))           return false;
-    if(ele->gsfTrack()->hitPattern().numberOfLostHits(reco::HitPattern::MISSING_INNER_HITS) >  1)   return false;
-    if(!ele->passConversionVeto())                                                                  return false;
-    return true;
-}
-
-// Trigger emulation for single electron triggers is available in VID
-// Trigger emulation for double electron triggers with CaloIdL_TrackIdL_IsoVL:
 bool LeptonAnalyzer::passTriggerEmulationDoubleEG(const pat::Electron* ele, const bool hOverE) const{
     float eInvMinusPInv =  (1.0 - ele->eSuperClusterOverP())/ele->correctedEcalEnergy();
     if(ele->full5x5_sigmaIetaIeta()                >= (ele->isEB() ? 0.011 : 0.030)) return false;
