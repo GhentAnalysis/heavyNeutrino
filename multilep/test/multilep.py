9e4141cc293e1a11ae76354390ecbab5b9ffbc26
tomc <tomc@cern.ch>
2019-03-25 12:02:53 +0100

    Remove outdated comment after 23f2480f015990731f18e0fb49dea8d731752a4b

--- multilep/test/multilep.py
+++ multilep/test/multilep.py
@@ -47,7 +47,7 @@ process.TFileService = cms.Service("TFileService", fileName = cms.string(outputF
 
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 if is2018 and 'PromptReco' in inputFile: process.GlobalTag.globaltag = '102X_dataRun2_Prompt_v13'
-elif is2018:                             process.GlobalTag.globaltag = '102X_dataRun2_Sep2018ABC_v2' if isData else '102X_upgrade2018_realistic_v18' # TO BE UPDATED REALLY SOON (NEW JEC)
+elif is2018:                             process.GlobalTag.globaltag = '102X_dataRun2_Sep2018ABC_v2' if isData else '102X_upgrade2018_realistic_v18'
 elif is2017:                             process.GlobalTag.globaltag = '94X_dataRun2_v11'            if isData else '94X_mc2017_realistic_v17'
 else:                                    process.GlobalTag.globaltag = '94X_dataRun2_v10'            if isData else '94X_mcRun2_asymptotic_v3'
 
