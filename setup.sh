ab57301e362757ef500c34b502f7ed9203d9daf1
willem <willem.verbeke@cern.ch>
2019-03-26 09:13:13 +0100

    update CMSSW version

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 # Setup script for branch: master
-RELEASE=CMSSW_10_2_10
+RELEASE=CMSSW_10_2_13
 BRANCH=master
 
 # If the release is already available using cmsenv, use it, otherwise set up a new one
