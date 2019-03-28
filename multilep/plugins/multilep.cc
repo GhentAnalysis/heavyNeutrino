GhentAnalysis/heavyNeutrino

commit 7d663e6bb89bbeb997ef6bad966a99aaa1d9a5ff

only run relevant lepton MVA versions, and make small step towards compartimentality of multilep class

multilep/plugins/multilep.cc
@@ -31,14 +31,14 @@ multilep::multilep(const edm::ParameterSet& iConfig):
     prefireWeightUpToken(             consumes<double>(                           edm::InputTag("prefiringweight:nonPrefiringProbUp"))),
     prefireWeightDownToken(           consumes<double>(                           edm::InputTag("prefiringweight:nonPrefiringProbDown"))),
     skim(                                                                         iConfig.getUntrackedParameter<std::string>("skim")),
-    isData(                                                                       iConfig.getUntrackedParameter<bool>("isData")),
-    is2017(                                                                       iConfig.getUntrackedParameter<bool>("is2017")),
-    is2018(                                                                       iConfig.getUntrackedParameter<bool>("is2018")),
-    isSUSY(                                                                       iConfig.getUntrackedParameter<bool>("isSUSY")),
+    sampleIsData(                                                                       iConfig.getUntrackedParameter<bool>("isData")),
+    sampleIs2017(                                                                       iConfig.getUntrackedParameter<bool>("is2017")),
+    sampleIs2018(                                                                       iConfig.getUntrackedParameter<bool>("is2018")),
+    sampleIsSUSY(                                                                       iConfig.getUntrackedParameter<bool>("isSUSY")),
     storeLheParticles(                                                            iConfig.getUntrackedParameter<bool>("storeLheParticles")),
     storeParticleLevel(                                                           iConfig.getUntrackedParameter<bool>("storeParticleLevel"))
 {
-    if(is2017 or is2018) ecalBadCalibFilterToken = consumes<bool>(edm::InputTag("ecalBadCalibReducedMINIAODFilter"));
+    if( is2017() || is2018() ) ecalBadCalibFilterToken = consumes<bool>(edm::InputTag("ecalBadCalibReducedMINIAODFilter"));
     triggerAnalyzer       = new TriggerAnalyzer(iConfig, this);
     leptonAnalyzer        = new LeptonAnalyzer(iConfig, this);
     photonAnalyzer        = new PhotonAnalyzer(iConfig, this);
@@ -73,16 +73,16 @@ void multilep::beginJob(){
     outputTree->Branch("_eventNb",                      &_eventNb,                      "_eventNb/l");
     outputTree->Branch("_nVertex",                      &_nVertex,                      "_nVertex/b");
 
-    if(!isData and !is2018){
-      outputTree->Branch("_prefireWeight",              &_prefireWeight,                "_prefireWeight/F");
-      outputTree->Branch("_prefireWeightUp",            &_prefireWeightUp,              "_prefireWeightUp/F");
-      outputTree->Branch("_prefireWeightDown",          &_prefireWeightDown,            "_prefireWeightDown/F");
+    if( isMC() && !is2018() ){
+        outputTree->Branch("_prefireWeight",              &_prefireWeight,                "_prefireWeight/F");
+        outputTree->Branch("_prefireWeightUp",            &_prefireWeightUp,              "_prefireWeightUp/F");
+        outputTree->Branch("_prefireWeightDown",          &_prefireWeightDown,            "_prefireWeightDown/F");
     }
 
-    if(!isData) lheAnalyzer->beginJob(outputTree, fs);
-    if(isSUSY)  susyMassAnalyzer->beginJob(outputTree, fs);
-    if(!isData) genAnalyzer->beginJob(outputTree);
-    if(!isData and storeParticleLevel) particleLevelAnalyzer->beginJob(outputTree);
+    if( isMC() ) lheAnalyzer->beginJob(outputTree, fs);
+    if( isSUSY() )  susyMassAnalyzer->beginJob(outputTree, fs);
+    if( isMC() ) genAnalyzer->beginJob(outputTree);
+    if( isMC() && storeParticleLevel) particleLevelAnalyzer->beginJob(outputTree);
     triggerAnalyzer->beginJob(outputTree);
     leptonAnalyzer->beginJob(outputTree);
     photonAnalyzer->beginJob(outputTree);
@@ -93,7 +93,7 @@ void multilep::beginJob(){
 
 // ------------ method called for each lumi block ---------
 void multilep::beginLuminosityBlock(const edm::LuminosityBlock& iLumi, const edm::EventSetup& iSetup){
-    if(isSUSY) susyMassAnalyzer->beginLuminosityBlock(iLumi, iSetup);
+    if( isSUSY() ) susyMassAnalyzer->beginLuminosityBlock(iLumi, iSetup);
     _lumiBlock = (unsigned long) iLumi.id().luminosityBlock();
 }
 
@@ -106,29 +106,29 @@ void multilep::beginRun(const edm::Run& iRun, edm::EventSetup const& iSetup){
 // ------------ method called for each event  ------------
 void multilep::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup){
     edm::Handle<std::vector<reco::Vertex>> vertices; iEvent.getByToken(vtxToken, vertices);
-    if(!isData) lheAnalyzer->analyze(iEvent);                                            // needs to be run before selection to get correct uncertainties on MC xsection
-    if(isSUSY) susyMassAnalyzer->analyze(iEvent);                                        // needs to be run after LheAnalyzer, but before all other models
+    if( isMC() ) lheAnalyzer->analyze(iEvent);                                            // needs to be run before selection to get correct uncertainties on MC xsection
+    if( isSUSY() ) susyMassAnalyzer->analyze(iEvent);                                        // needs to be run after LheAnalyzer, but before all other models
 
     _nVertex = vertices->size();
     nVertices->Fill(_nVertex, lheAnalyzer->getWeight()); 
 
-    bool skim;                                                                           // Do not skim if event topology is available on particleLevel
-    if(!isData and storeParticleLevel) skim = !particleLevelAnalyzer->analyze(iEvent);
-    else                               skim = true;
+    bool applySkim; //better not to shadow class variable with name! // Do not skim if event topology is available on particleLevel 
+    if( isMC() && storeParticleLevel ) applySkim = !particleLevelAnalyzer->analyze(iEvent);
+    else applySkim = true;
 
-    if(_nVertex == 0 and skim)                                          return;          // Don't consider 0 vertex events
-    if(!leptonAnalyzer->analyze(iEvent, *(vertices->begin())) and skim) return;          // returns false if doesn't pass skim condition, so skip event in such case
-    if(!photonAnalyzer->analyze(iEvent) and skim)                       return;
-    if(!jetAnalyzer->analyze(iEvent) and skim)                          return;
-    if(!isData) genAnalyzer->analyze(iEvent);
+    if(_nVertex == 0 and applySkim)                                          return;          // Don't consider 0 vertex events
+    if(!leptonAnalyzer->analyze(iEvent, *(vertices->begin())) and applySkim) return;          // returns false if doesn't pass applySkim condition, so skip event in such case
+    if(!photonAnalyzer->analyze(iEvent) and applySkim)                       return;
+    if(!jetAnalyzer->analyze(iEvent) and applySkim)                          return;
+    if( isMC() ) genAnalyzer->analyze(iEvent);
     triggerAnalyzer->analyze(iEvent);
 
     _eventNb = (unsigned long) iEvent.id().event();
 
-    if(!isData and !is2018){
-      edm::Handle<double> pfw;     iEvent.getByToken(prefireWeightToken,     pfw);     _prefireWeight     = (*pfw);
-      edm::Handle<double> pfwUp;   iEvent.getByToken(prefireWeightUpToken,   pfwUp);   _prefireWeightUp   = (*pfwUp);
-      edm::Handle<double> pfwDown; iEvent.getByToken(prefireWeightDownToken, pfwDown); _prefireWeightDown = (*pfwDown);
+    if( isMC() && is2018() ){
+        edm::Handle<double> pfw;     iEvent.getByToken(prefireWeightToken,     pfw);     _prefireWeight     = (*pfw);
+        edm::Handle<double> pfwUp;   iEvent.getByToken(prefireWeightUpToken,   pfwUp);   _prefireWeightUp   = (*pfwUp);
+        edm::Handle<double> pfwDown; iEvent.getByToken(prefireWeightDownToken, pfwDown); _prefireWeightDown = (*pfwDown);
     }
 
     outputTree->Fill();                                                                  //store calculated event info in root tree

multilep/interface/LeptonAnalyzer.h
@@ -126,12 +126,9 @@ class LeptonAnalyzer {
     double _tauIsoMVAPWoldDMwLT[nL_max];
 
     //lepton MVA definitions for SUSY (ewkino), TTH and tZq 
-    double _leptonMvaSUSY16[nL_max];
-    double _leptonMvaTTH16[nL_max];
-    double _leptonMvaSUSY17[nL_max];
-    double _leptonMvaTTH17[nL_max];
-    double _leptonMvatZqTTV16[nL_max];
-    double _leptonMvatZqTTV17[nL_max];
+    double _leptonMvaSUSY[nL_max];
+    double _leptonMvaTTH[nL_max];
+    double _leptonMvatZq[nL_max];
 
     //analysis specific lepton selection (please do NOT delete for now, will be deleted when analysis code is updated 
     bool _lEwkLoose[nL_max];
@@ -195,12 +192,9 @@ class LeptonAnalyzer {
     double leptonMvaVal(const pat::Electron&, LeptonMvaHelper*);
 
     //for lepton MVA calculation
-    LeptonMvaHelper* leptonMvaComputerSUSY16;
-    LeptonMvaHelper* leptonMvaComputerTTH16;
-    LeptonMvaHelper* leptonMvaComputerSUSY17;
-    LeptonMvaHelper* leptonMvaComputerTTH17;
-    LeptonMvaHelper* leptonMvaComputertZqTTV16;
-    LeptonMvaHelper* leptonMvaComputertZqTTV17;
+    LeptonMvaHelper* leptonMvaComputerSUSY;
+    LeptonMvaHelper* leptonMvaComputerTTH;
+    LeptonMvaHelper* leptonMvaComputertZq;
 
   public:
     LeptonAnalyzer(const edm::ParameterSet& iConfig, multilep* vars);

multilep/plugins/multilep.h
@@ -66,6 +66,14 @@ class multilep : public edm::one::EDAnalyzer<edm::one::WatchLuminosityBlocks, ed
     friend SUSYMassAnalyzer;
     public:
         explicit multilep(const edm::ParameterSet&);
+
+        bool isData() const{ return sampleIsData; }
+        bool isMC() const{ return !sampleIsData; }
+        bool is2016() const{ return !(sampleIs2017 || sampleIs2018); }
+        bool is2017() const{ return sampleIs2017; }
+        bool is2018() const{ return sampleIs2018; }
+        bool isSUSY() const{ return sampleIsSUSY; }
+
         ~multilep();
 
     private:
@@ -99,10 +107,10 @@ class multilep : public edm::one::EDAnalyzer<edm::one::WatchLuminosityBlocks, ed
         edm::EDGetTokenT<double>                            prefireWeightUpToken;
         edm::EDGetTokenT<double>                            prefireWeightDownToken;
         std::string                                         skim;
-        bool                                                isData;
-        bool                                                is2017;
-        bool                                                is2018;
-        bool                                                isSUSY;
+        bool                                                sampleIsData;
+        bool                                                sampleIs2017;
+        bool                                                sampleIs2018;
+        bool                                                sampleIsSUSY;
         bool                                                storeLheParticles;
         bool                                                storeParticleLevel;
 

multilep/src/JetAnalyzer.cc
@@ -66,7 +66,7 @@ void JetAnalyzer::beginJob(TTree* outputTree){
     outputTree->Branch("_metPhiUnclUp",                 &_metPhiUnclUp,                 "_metPhiUnclUp/D");
     outputTree->Branch("_metSignificance",              &_metSignificance,              "_metSignificance/D");
 
-    if(!multilepAnalyzer->is2018) outputTree->Branch("_jetIsLoose", _jetIsLoose, "_jetIsLoose[_nJets]/O"); // WARNING, not recommended to be used, only exists for 2016
+    if(!multilepAnalyzer->is2018() ) outputTree->Branch("_jetIsLoose", _jetIsLoose, "_jetIsLoose[_nJets]/O"); // WARNING, not recommended to be used, only exists for 2016
 }
 
 bool JetAnalyzer::analyze(const edm::Event& iEvent){
@@ -82,9 +82,9 @@ bool JetAnalyzer::analyze(const edm::Event& iEvent){
     for(const auto& jet : *jets){
         if(_nJets == nJets_max) break;
 
-        _jetIsLoose[_nJets]        = jetIsLoose(jet, multilepAnalyzer->is2017 || multilepAnalyzer->is2018);
-        _jetIsTight[_nJets]        = jetIsTight(jet, multilepAnalyzer->is2017, multilepAnalyzer->is2018);
-        _jetIsTightLepVeto[_nJets] = jetIsTightLepVeto(jet, multilepAnalyzer->is2017, multilepAnalyzer->is2018);
+        _jetIsLoose[_nJets]        = jetIsLoose(jet, multilepAnalyzer->is2017() || multilepAnalyzer->is2018() );
+        _jetIsTight[_nJets]        = jetIsTight(jet, multilepAnalyzer->is2017(), multilepAnalyzer->is2018() );
+        _jetIsTightLepVeto[_nJets] = jetIsTightLepVeto(jet, multilepAnalyzer->is2017(), multilepAnalyzer->is2018() );
 
         //find smeared equivalents of nominal jet
         auto jetSmearedIt = jetsSmeared->begin();

multilep/src/LeptonAnalyzer.cc
@@ -9,23 +9,17 @@
 LeptonAnalyzer::LeptonAnalyzer(const edm::ParameterSet& iConfig, multilep* multilepAnalyzer):
     multilepAnalyzer(multilepAnalyzer),
     electronsEffectiveAreas(iConfig.getParameter<edm::FileInPath>("electronsEffectiveAreas").fullPath()),
-    muonsEffectiveAreas    ((multilepAnalyzer->is2017 || multilepAnalyzer->is2018)? (iConfig.getParameter<edm::FileInPath>("muonsEffectiveAreasFall17")).fullPath() : (iConfig.getParameter<edm::FileInPath>("muonsEffectiveAreas")).fullPath() )
+    muonsEffectiveAreas    ((multilepAnalyzer->is2017() || multilepAnalyzer->is2018() )? (iConfig.getParameter<edm::FileInPath>("muonsEffectiveAreasFall17")).fullPath() : (iConfig.getParameter<edm::FileInPath>("muonsEffectiveAreas")).fullPath() )
 {
-    leptonMvaComputerSUSY16   = new LeptonMvaHelper(iConfig, 0, false); // SUSY         // TODO: all of these really needed? could use some clean-up
-    leptonMvaComputerTTH16    = new LeptonMvaHelper(iConfig, 1, false); // TTH
-    leptonMvaComputerSUSY17   = new LeptonMvaHelper(iConfig, 0, true);  // SUSY
-    leptonMvaComputerTTH17    = new LeptonMvaHelper(iConfig, 1, true);  // TTH
-    leptonMvaComputertZqTTV16 = new LeptonMvaHelper(iConfig, 2, false); // tZq/TTV
-    leptonMvaComputertZqTTV17 = new LeptonMvaHelper(iConfig, 2, true);  // tZq/TTV
+    leptonMvaComputerSUSY = new LeptonMvaHelper(iConfig, 0, !multilepAnalyzer->is2016() ); // SUSY 
+    leptonMvaComputerTTH = new LeptonMvaHelper(iConfig, 1, !multilepAnalyzer->is2016() );
+    leptonMvaComputertZq = new LeptonMvaHelper(iConfig, 2, !multilepAnalyzer->is2016() );
 };
 
 LeptonAnalyzer::~LeptonAnalyzer(){
-    delete leptonMvaComputerSUSY16;
-    delete leptonMvaComputerTTH16;
-    delete leptonMvaComputertZqTTV16;
-    delete leptonMvaComputerSUSY17;
-    delete leptonMvaComputerTTH17;
-    delete leptonMvaComputertZqTTV17;
+    delete leptonMvaComputerSUSY;
+    delete leptonMvaComputerTTH;
+    delete leptonMvaComputertZq;
 }
 
 void LeptonAnalyzer::beginJob(TTree* outputTree){
@@ -54,12 +48,9 @@ void LeptonAnalyzer::beginJob(TTree* outputTree){
     outputTree->Branch("_lElectronPassConvVeto",        &_lElectronPassConvVeto,        "_lElectronPassConvVeto[_nLight]/O");
     outputTree->Branch("_lElectronChargeConst",         &_lElectronChargeConst,         "_lElectronChargeConst[_nLight]/O");
     outputTree->Branch("_lElectronMissingHits",         &_lElectronMissingHits,         "_lElectronMissingHits[_nLight]/i");
-    outputTree->Branch("_leptonMvaSUSY16",              &_leptonMvaSUSY16,              "_leptonMvaSUSY16[_nLight]/D");
-    outputTree->Branch("_leptonMvaTTH16",               &_leptonMvaTTH16,               "_leptonMvaTTH16[_nLight]/D");
-    outputTree->Branch("_leptonMvaSUSY17",              &_leptonMvaSUSY17,              "_leptonMvaSUSY17[_nLight]/D");
-    outputTree->Branch("_leptonMvaTTH17",               &_leptonMvaTTH17,               "_leptonMvaTTH17[_nLight]/D");
-    outputTree->Branch("_leptonMvatZqTTV16",            &_leptonMvatZqTTV16,            "_leptonMvatZqTTV16[_nLight]/D");
-    outputTree->Branch("_leptonMvatZqTTV17",            &_leptonMvatZqTTV17,            "_leptonMvatZqTTV17[_nLight]/D");
+    outputTree->Branch("_leptonMvaSUSY",                &_leptonMvaSUSY,                "_leptonMvaSUSY[_nLight]/D");
+    outputTree->Branch("_leptonMvaTTH",                 &_leptonMvaTTH,                 "_leptonMvaTTH[_nLight]/D");
+    outputTree->Branch("_leptonMvatZq",                 &_leptonMvatZq,                 "_leptonMvatZq[_nLight]/D");
     outputTree->Branch("_lEwkLoose",                    &_lEwkLoose,                    "_lEwkLoose[_nL]/O");
     outputTree->Branch("_lEwkFO",                       &_lEwkFO,                       "_lEwkFO[_nL]/O");
     outputTree->Branch("_lEwkTight",                    &_lEwkTight,                    "_lEwkTight[_nL]/O");
@@ -97,7 +88,7 @@ void LeptonAnalyzer::beginJob(TTree* outputTree){
     outputTree->Branch("_lMuonSegComp",                 &_lMuonSegComp,                 "_lMuonSegComp[_nMu]/D");
     outputTree->Branch("_lMuonTrackPt",                 &_lMuonTrackPt,                 "_lMuonTrackPt[_nMu]/D");
     outputTree->Branch("_lMuonTrackPtErr",              &_lMuonTrackPtErr,              "_lMuonTrackPtErr[_nMu]/D");
-    if(!multilepAnalyzer->isData){
+    if( multilepAnalyzer->isMC() ){
       outputTree->Branch("_lIsPrompt",                  &_lIsPrompt,                    "_lIsPrompt[_nL]/O");
       outputTree->Branch("_lMatchPdgId",                &_lMatchPdgId,                  "_lMatchPdgId[_nL]/I");
       outputTree->Branch("_lMomPdgId",                  &_lMomPdgId,                    "_lMomPdgId[_nL]/I");
@@ -105,7 +96,7 @@ void LeptonAnalyzer::beginJob(TTree* outputTree){
       outputTree->Branch("_lProvenanceCompressed",      &_lProvenanceCompressed,        "_lProvenanceCompressed[_nL]/i");
       outputTree->Branch("_lProvenanceConversion",      &_lProvenanceConversion,        "_lProvenanceConversion[_nL]/i");
     }
-    if(!multilepAnalyzer->is2018){
+    if( !multilepAnalyzer->is2018() ){
       outputTree->Branch("_lPtCorr",                    &_lPtCorr,                      "_lPtCorr[_nLight]/D");
       outputTree->Branch("_lPtScaleUp",                 &_lPtScaleUp,                   "_lPtScaleUp[_nLight]/D");
       outputTree->Branch("_lPtScaleDown",               &_lPtScaleDown,                 "_lPtScaleDown[_nLight]/D");
@@ -148,7 +139,7 @@ bool LeptonAnalyzer::analyze(const edm::Event& iEvent, const reco::Vertex& prima
         if(fabs(_dxy[_nL]) > 0.05)                     continue;
         if(fabs(_dz[_nL]) > 0.1)                       continue;
         fillLeptonKinVars(mu);
-        if(!multilepAnalyzer->isData) fillLeptonGenVars(mu, *genParticles);
+        if( multilepAnalyzer->isMC() ) fillLeptonGenVars(mu, *genParticles);
         fillLeptonJetVariables(mu, jets, primaryVertex, *rho);
 
         _lFlavor[_nL]        = 1;
@@ -166,14 +157,10 @@ bool LeptonAnalyzer::analyze(const edm::Event& iEvent, const reco::Vertex& prima
         _lPOGLoose[_nL]      = mu.passed(reco::Muon::CutBasedIdLoose);
         _lPOGMedium[_nL]     = mu.passed(reco::Muon::CutBasedIdMedium);
         _lPOGTight[_nL]      = mu.passed(reco::Muon::CutBasedIdTight);
-        // TODO: consider to add muon MVA
 
-        _leptonMvaSUSY16[_nL]  = leptonMvaVal(mu, leptonMvaComputerSUSY16);
-        _leptonMvaTTH16[_nL]   = leptonMvaVal(mu, leptonMvaComputerTTH16);
-        _leptonMvaSUSY17[_nL]  = leptonMvaVal(mu, leptonMvaComputerSUSY17);
-        _leptonMvaTTH17[_nL]   = leptonMvaVal(mu, leptonMvaComputerTTH17);
-        _leptonMvatZqTTV16[_nL] = leptonMvaVal(mu, leptonMvaComputertZqTTV16);
-        _leptonMvatZqTTV17[_nL] = leptonMvaVal(mu, leptonMvaComputertZqTTV17);
+        _leptonMvaSUSY[_nL]  = leptonMvaVal(mu, leptonMvaComputerSUSY);
+        _leptonMvaTTH[_nL]   = mu.mvaValue();
+        _leptonMvatZq[_nL]   = leptonMvaVal(mu, leptonMvaComputertZq);
 
         _lEwkLoose[_nL]      = isEwkLoose(mu);
         _lEwkFO[_nL]         = isEwkFO(mu);
@@ -195,7 +182,7 @@ bool LeptonAnalyzer::analyze(const edm::Event& iEvent, const reco::Vertex& prima
         if(fabs(_dxy[_nL]) > 0.05)                                                                      continue;
         if(fabs(_dz[_nL]) > 0.1)                                                                        continue;
         fillLeptonKinVars(*ele);
-        if(!multilepAnalyzer->isData) fillLeptonGenVars(*ele, *genParticles);
+        if( multilepAnalyzer->isMC() ) fillLeptonGenVars(*ele, *genParticles);
         fillLeptonJetVariables(*ele, jets, primaryVertex, *rho);
 
         _lFlavor[_nL]                   = 0;
@@ -220,12 +207,9 @@ bool LeptonAnalyzer::analyze(const edm::Event& iEvent, const reco::Vertex& prima
         _lPOGMedium[_nL]                = ele->electronID("cutBasedElectronID-Fall17-94X-V1-medium");
         _lPOGTight[_nL]                 = ele->electronID("cutBasedElectronID-Fall17-94X-V1-tight");
 
-        _leptonMvaSUSY16[_nL]           = leptonMvaVal(*ele, leptonMvaComputerSUSY16);
-        _leptonMvaTTH16[_nL]            = leptonMvaVal(*ele, leptonMvaComputerTTH16);
-        _leptonMvaSUSY17[_nL]           = leptonMvaVal(*ele, leptonMvaComputerSUSY17);
-        _leptonMvaTTH17[_nL]            = leptonMvaVal(*ele, leptonMvaComputerTTH17);
-        _leptonMvatZqTTV16[_nL]         = leptonMvaVal(*ele, leptonMvaComputertZqTTV16);
-        _leptonMvatZqTTV17[_nL]         = leptonMvaVal(*ele, leptonMvaComputertZqTTV17);
+        _leptonMvaSUSY[_nL]             = leptonMvaVal(*ele, leptonMvaComputerSUSY);
+        _leptonMvaTTH[_nL]              = leptonMvaVal(*ele, leptonMvaComputerTTH);
+        _leptonMvatZq[_nL]              = leptonMvaVal(*ele, leptonMvaComputertZq);
 
         _lEwkLoose[_nL]                 = isEwkLoose(*ele);
         _lEwkFO[_nL]                    = isEwkFO(*ele);
@@ -235,7 +219,7 @@ bool LeptonAnalyzer::analyze(const edm::Event& iEvent, const reco::Vertex& prima
         // In case these systematics turn out to be important, need to add their individual source to the tree (and propagate to their own templates):
         // https://twiki.cern.ch/twiki/bin/viewauth/CMS/EgammaMiniAODV2#Energy_Scale_and_Smearing
         // Currently only available for 2016/2017
-        if(!multilepAnalyzer->is2018){
+        if( !multilepAnalyzer->is2018() ){
           _lPtCorr[_nL]                 = ele->pt()*ele->userFloat("ecalTrkEnergyPostCorr")/ele->energy();
           _lPtScaleUp[_nL]              = ele->pt()*ele->userFloat("energyScaleUp")/ele->energy();
           _lPtScaleDown[_nL]            = ele->pt()*ele->userFloat("energyScaleDown")/ele->energy();
@@ -269,7 +253,7 @@ bool LeptonAnalyzer::analyze(const edm::Event& iEvent, const reco::Vertex& prima
         if(fabs(tau.eta()) > 2.3) continue;
         //if(!tau.tauID("decayModeFinding")) continue;
         fillLeptonKinVars(tau);
-        if(!multilepAnalyzer->isData) fillLeptonGenVars(tau, *genParticles);
+        if( multilepAnalyzer->isMC() ) fillLeptonGenVars(tau, *genParticles);
         fillLeptonImpactParameters(tau, primaryVertex);
 
         _lFlavor[_nL]                   = 2;

multilep/src/LeptonAnalyzerId.cc
@@ -158,7 +158,7 @@ bool LeptonAnalyzer::isEwkFO(const pat::Muon& lep) const{
     if(!_lEwkLoose[_nL])        return false;
     if(_lPt[_nL] <= 10)         return false;
     if(!lep.isMediumMuon())     return false;
-    return _leptonMvaSUSY16[_nL] > -0.2 || (_ptRatio[_nL] > 0.3 && _closestJetCsvV2[_nL] < 0.3);
+    return _leptonMvaSUSY[_nL] > -0.2 || (_ptRatio[_nL] > 0.3 && _closestJetCsvV2[_nL] < 0.3);
 }
 
 bool LeptonAnalyzer::isEwkFO(const pat::Electron& lep) const{
@@ -167,11 +167,11 @@ bool LeptonAnalyzer::isEwkFO(const pat::Electron& lep) const{
     if(!passTriggerEmulationDoubleEG(&lep, false))                                                  return false;
     if(lep.gsfTrack()->hitPattern().numberOfLostHits(reco::HitPattern::MISSING_INNER_HITS) !=0)     return false;
     double ptCone = _lPt[_nL];
-    if(_leptonMvaSUSY16[_nL] <= 0.5){
+    if(_leptonMvaSUSY[_nL] <= 0.5){
         ptCone *= 0.85/_ptRatio[_nL];
     }
     if(ptCone >= 30 && lep.hadronicOverEm() >= (lep.isEB() ? 0.10  : 0.07) )                        return false;
-    return _leptonMvaSUSY16[_nL] > 0.5 || (passElectronMvaEwkFO(&lep, _lElectronMvaSummer16GP[_nL]) && _ptRatio[_nL] > 0.3 && _closestJetCsvV2[_nL] < 0.3);
+    return _leptonMvaSUSY[_nL] > 0.5 || (passElectronMvaEwkFO(&lep, _lElectronMvaSummer16GP[_nL]) && _ptRatio[_nL] > 0.3 && _closestJetCsvV2[_nL] < 0.3);
 }
 
 bool LeptonAnalyzer::isEwkFO(const pat::Tau& tau) const{
@@ -180,14 +180,14 @@ bool LeptonAnalyzer::isEwkFO(const pat::Tau& tau) const{
 
 bool LeptonAnalyzer::isEwkTight(const pat::Muon& lep) const{
     if(!_lEwkFO[_nL]) return false;
-    return _leptonMvaSUSY16[_nL] > -0.2;
+    return _leptonMvaSUSY[_nL] > -0.2;
 }
 
 bool LeptonAnalyzer::isEwkTight(const pat::Electron& lep) const{
     if(!_lEwkFO[_nL])                       return false;
     if(!passTriggerEmulationDoubleEG(&lep)) return false;
     if(!lep.passConversionVeto())           return false;
-    return _leptonMvaSUSY16[_nL] > 0.5;
+    return _leptonMvaSUSY[_nL] > 0.5;
 }
 
 bool LeptonAnalyzer::isEwkTight(const pat::Tau& tau) const{

multilep/src/LheAnalyzer.cc
@@ -17,7 +17,7 @@ LheAnalyzer::LheAnalyzer(const edm::ParameterSet& iConfig, multilep* multilepAna
 
 
 void LheAnalyzer::beginJob(TTree* outputTree, edm::Service<TFileService>& fs){
-    if(multilepAnalyzer->isData) return;
+    if( multilepAnalyzer->isData() ) return;
     hCounter   = fs->make<TH1D>("hCounter",   "Events counter", 1, 0, 1);
     lheCounter = fs->make<TH1D>("lheCounter", "Lhe weights",    110, 0, 110); //Counter to determine effect of pdf and scale uncertainties on the MC cross section
     psCounter  = fs->make<TH1D>("psCounter",  "Lhe weights",    14, 0, 14);
@@ -50,7 +50,7 @@ void LheAnalyzer::beginJob(TTree* outputTree, edm::Service<TFileService>& fs){
 }
 
 void LheAnalyzer::analyze(const edm::Event& iEvent){
-    if(multilepAnalyzer->isData) return;
+    if( multilepAnalyzer->isData() ) return;
 
     edm::Handle<GenEventInfoProduct> genEventInfo;          iEvent.getByToken(multilepAnalyzer->genEventInfoToken, genEventInfo);
     edm::Handle<LHEEventProduct> lheEventInfo;              iEvent.getByToken(multilepAnalyzer->lheEventInfoToken, lheEventInfo);
@@ -115,7 +115,7 @@ void LheAnalyzer::analyze(const edm::Event& iEvent){
 }
 
 double LheAnalyzer::getWeight() const{
-    if(multilepAnalyzer->isData) return 1.;
+    if( multilepAnalyzer->isData() ) return 1.;
     return _weight;
 }
 

commit 7db93f13188ca789ad12ce2c9a6cc6276e201765

Bug fix for prefiring weights
Fixing bug introduced in 7d663e6bb89bbeb997ef6bad966a99aaa1d9a5ff

multilep/plugins/multilep.cc
@@ -125,7 +125,7 @@ void multilep::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup){
 
     _eventNb = (unsigned long) iEvent.id().event();
 
-    if( isMC() && is2018() ){
+    if(isMC() and !is2018()){
         edm::Handle<double> pfw;     iEvent.getByToken(prefireWeightToken,     pfw);     _prefireWeight     = (*pfw);
         edm::Handle<double> pfwUp;   iEvent.getByToken(prefireWeightUpToken,   pfwUp);   _prefireWeightUp   = (*pfwUp);
         edm::Handle<double> pfwDown; iEvent.getByToken(prefireWeightDownToken, pfwDown); _prefireWeightDown = (*pfwDown);