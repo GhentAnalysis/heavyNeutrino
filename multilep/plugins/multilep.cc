ee63574b5baf954cfb8390e2dfff58aac8e3899b
willem <willem.verbeke@cern.ch>
2019-08-28 12:09:14 +0200

    store nVertex as unsigned integer

--- multilep/plugins/multilep.cc
+++ multilep/plugins/multilep.cc
@@ -73,7 +73,7 @@ void multilep::beginJob(){
     outputTree->Branch("_runNb",                        &_runNb,                        "_runNb/l");
     outputTree->Branch("_lumiBlock",                    &_lumiBlock,                    "_lumiBlock/l");
     outputTree->Branch("_eventNb",                      &_eventNb,                      "_eventNb/l");
-    outputTree->Branch("_nVertex",                      &_nVertex,                      "_nVertex/b");
+    outputTree->Branch("_nVertex",                      &_nVertex,                      "_nVertex/i");
     outputTree->Branch("_is2017",                       &sampleIs2017,                  "_is2017/O");
     outputTree->Branch("_is2018",                       &sampleIs2018,                  "_is2018/O");
 
