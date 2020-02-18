6ba1b737e0236030b34e94229c98352f5e00081c
bvermass <basilevermassen@hotmail.com>
2020-02-18 10:27:23 +0100

    script to report killed crab jobs (report makes notfinishedlumis.json that can be used for resubmitting data in new jobs)

---
+++ multilep/test/scripts/reportCrabJobs.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+echo "reporting all running crab jobs listed in ../crab directory"
+cd ../crab
+for dir in *
+    do if [[ -d $dir ]]; then
+        cd $dir
+        for dir2 in *
+            do if [[ -d ${dir2} ]]; then
+                cd ${dir2}
+                for dir3 in *
+                    do if [[ -d ${dir3} ]]; then
+                        cd ${dir3}
+                        crab report --dir=./
+                        cd ..
+                    fi
+                done
+                cd ..
+            fi
+        done
+        cd ..
+    fi
+done
