76943e46e05f55aedabc179059f58e621205a1a3
bvermass <basilevermassen@hotmail.com>
2021-02-18 19:12:56 +0100

    Add 2017 single muon triggers

--- multilep/plugins/multilepFilter.cc
+++ multilep/plugins/multilepFilter.cc
@@ -67,7 +67,7 @@ bool multilepFilter::filter(edm::Event& iEvent, const edm::EventSetup& iSetup){
 
     // Trigger
     std::vector<std::string> triggers;
-    if(sampleIs2018 or sampleIs2017) triggers = {"HLT_Ele32_WPTight_Gsf", "HLT_IsoMu24"};
+    if(sampleIs2018 or sampleIs2017) triggers = {"HLT_Ele32_WPTight_Gsf", "HLT_IsoMu24", "HLT_IsoMu27", "HLT_IsoMu24_eta2p1"};
     else                             triggers = {"HLT_Ele27_WPTight_Gsf", "HLT_IsoMu24", "HLT_IsoTkMu24"};
 
     bool passedTrigger = false;
