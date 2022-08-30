10534a522334b453756d2c6d5ab52515deb0153e
bvermass <basilevermassen@hotmail.com>
2022-08-30 17:40:19 +0200

    correct location of lumimasks, catch Fall17 exception identifying string, add two scripts

--- multilep/test/crab.py
+++ multilep/test/crab.py
@@ -14,6 +14,7 @@ requestName = dataset.split('/')[2] + '_' + productionLabel
 requestName = requestName.replace('RunIISummer16MiniAODv3-PUMoriond17_94X_mcRun2_asymptotic_v3', 'MiniAOD2016v3')
 requestName = requestName.replace('RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14', 'MiniAOD2017v2')
 requestName = requestName.replace('RunIIFall17MiniAODv2-PU2017_12Apr2018_new_pmx_94X_mc2017_realistic_v14', 'MiniAOD2017v2NewPMX')
+requestName = requestName.replace('RunIIFall17MiniAODv2-PU2017RECOSIMstep_12Apr2018_94X_mc2017_realistic_v14', 'MiniAOD2017v2')
 requestName = requestName.replace('RunIIAutumn18MiniAOD-102X_upgrade2018_realistic_v15', 'MiniAOD2018')
 
 # Crab configuration
---
+++ multilep/test/scripts/get_files.py
@@ -0,0 +1,18 @@
+import os,sys,subprocess, glob, time
+def system(command):
+  try:
+    return subprocess.check_output(command, shell=True, stderr=subprocess.STDOUT).decode()
+  except subprocess.CalledProcessError as e:
+    print(e.output)
+    return 'error'
+
+search_string = sys.argv[1]
+output = system('dasgoclient --query="dataset='+search_string+'"')
+samples = output.split('\n')
+samples = [sample for sample in samples if sample]
+
+for sample in samples:
+    output = system('dasgoclient -query="file dataset='+sample+'"').split('\n')
+    output = [line.strip().split() for line in output if line and not '[' in line][0]
+    output = [line for line in output if line.isalnum()]
+    print('{}: {}'.format(sample, output[0]))
